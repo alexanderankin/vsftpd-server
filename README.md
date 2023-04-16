5f16526aaf87ad392226e9f415740ef5ea0405bf
Dave Ankin <daveankin@gmail.com>
2023-04-16 17:20:27 -0400

    pasv_promiscuous, users, custom pasv port range

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # vsftpd-server
+
+## environment variables
+
+### `FTP_CUSTOMIZATION`
+
+turn on any additional functionality beyond the ubuntu image with vsftp installed.
+
+the entrypoint checks for the presence of this variable at startup and starts `vsftpd` with the customized config file.
+
+### `FTP_PASV_MAX_PORT`
+
+edit the `pasv_max_port` entry in the customized config file before starting.
+
+this restricts the passive port range.
+
+### `FTP_PASV_MIN_PORT`
+
+see `FTP_PASV_MAX_PORT`.
+
+### `FTP_USER`/`FTP_PASS`
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ ! -z ${FTP_CUSTOMIZATION} ]]; then
+	[[ -z ${FTP_PASV_MAX_PORT} ]] || sed -i "s/pasv_max_port.*/pasv_max_port=$FTP_PASV_MAX_PORT/" /etc/vsftpd-custom.conf
+	[[ -z ${FTP_PASV_MIN_PORT} ]] || sed -i "s/pasv_min_port.*/pasv_min_port=$FTP_PASV_MIN_PORT/" /etc/vsftpd-custom.conf
+
+	[[ -z ${FTP_USER} ]] || adduser --disabled-password --gecos "" $FTP_USER > /dev/null \
+		&& echo created user \
+		|| echo creating user failed
+	[[ -z ${FTP_PASS} ]] || chpasswd <<< $FTP_USER:$FTP_PASS
+
+	echo starting custom server
 	vsftpd /etc/vsftpd-custom.conf
 else
+	echo starting server
 	vsftpd /etc/vsftpd.conf
 fi
